f46271045870a8a147eaeb07282180a47e587691
ykworld <yonseung.kim@gmail.com>
2017-06-01 17:43:58 +0000

    finished Closures Stretch

---
+++ logginWrapper.js
@@ -0,0 +1,15 @@
+var wrapLog = function (callback, name) {
+  /* your code here */
+  return function() {
+    var a = arguments[0];
+    var b = arguments[1];
+    var sum = callback(a, b);
+    console.log("sum(" + a + "," + b + ") => " + sum);
+  }
+};
+
+var sum = function (a,b) { return a + b; };
+var logSum = wrapLog(sum, "sum");
+
+logSum(5,3); // sum(5,3) => 8
+logSum(3,2); // sum(3,2) => 5
